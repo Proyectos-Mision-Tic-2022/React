062ccbcc5221bd04ad95ae78cdb00cbb01aef9ae
Arley Rivas G <arrivasg@unipacifico.edu.co>
2021-12-14 02:17:37 -0500

    Commit 1, componentes basicos en react

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import "./styles/styles.css";
+import logo from "./media/logo.png";
+import borderCollie from "./media/borderCollie.jpg";
+import rhodesian from "./media/rhodesian.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div>hola mundo, esto es un div en react</div>
+        <ul className="navbar">
+          <li>
+            <img src={logo} alt="imagen" className="logo" />
+          </li>
+          <li>
+            <button className="botonGenerico mainButton">Nuevo post</button>
+          </li>
+          <li>
+            <div className="buscar">
+              <input placeholder="Buscar una raza" />
+              <i className="fas fa-search botonGenerico iconoBusqueda"></i>
+            </div>
+          </li>
+          <li>
+            <button className="botonGenerico secondaryButton">Login</button>
+          </li>
+          <li>
+            <button className="botonGenerico mainButton">Registro</button>
+          </li>
+        </ul>
+      </header>
+      <main>
+        <section>
+          <h1>Razas de Perros</h1>
+          <ul className="breedCardContainer">
+            <CardRazasPerros />
+            <CardRazasPerros />
+          </ul>
+        </section>
+        <section></section>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+function CardRazasPerros() {
+  return (
+    <li className="breedCard">
+      <div className="contenedorImagen">
+        <img src={borderCollie} alt="Border Collie" />
+      </div>
+      <span className="breedTitle">Border Collie </span>
+    </li>
+  );
+}
+
+export default App;
