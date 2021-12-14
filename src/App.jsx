9acc4111398810d923ad400b9b7fdcdb7173b5f1
Arley Rivas G <arrivasg@unipacifico.edu.co>
2021-12-14 11:48:21 -0500

    agrego react Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,17 @@
-import CardRazasPerros from 'components/CardRazasPerros';
+import Index from "pages";
 import "styles/styles.css";
-import logo from "media/logo.png";
-import borderCollie from "media/borderCollie.jpg";
-import rhodesian from "media/rhodesian.jpg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        
-        <ul className="navbar">
-          <li>
-            <img src={logo} alt="imagen" className="logo" />
-          </li>
-          <li>
-            <button className="botonGenerico mainButton">Nuevo post</button>
-          </li>
-          <li>
-            <div className="buscar">
-              <input placeholder="Buscar una raza" />
-              <i className="fas fa-search botonGenerico iconoBusqueda"></i>
-            </div>
-          </li>
-          <li>
-            <button className="botonGenerico secondaryButton">Login</button>
-          </li>
-          <li>
-            <button className="botonGenerico mainButton">Registro</button>
-          </li>
-        </ul>
-      </header>
-      <main>
-        <section>
-          <h1>Razas de Perros</h1>
-          <ul className="breedCardContainer">
-            <CardRazasPerros nombreRaza="BorderCollie" imagen={borderCollie} color='backGroundVerde'/>
-            <CardRazasPerros nombreRaza="Rhodesain" imagen={rhodesian} color='backGroundRojo'/>
-            <CardRazasPerros nombreRaza="Rhodesain" imagen={rhodesian} color='backGroundRojo' />
-          </ul>
-        </section>
-        <section></section>
-      </main>
-      <footer></footer>
+      <Router>
+        <Switch>
+          <Route path="/index">
+            <Index />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/rhodesian.jsx
@@ -0,0 +1,8 @@
+function rhodesian(){
+
+    return (
+
+        <div><p>Esta es la pagina del info del rhodesain</p></div>
+    );
+}
+export default rhodesian();
